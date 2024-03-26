77327b32d4473ccd3a15e68e2bb833856fd4bcfd
andrija331 <andrija.panovic333@gmail.com>
2024-03-26 17:35:45 +0100

    Dodate toString i equals metode u klase Autor i Knjiga

--- Biblioteka_moja/src/bibilioteka/Knjiga.java
+++ Biblioteka_moja/src/bibilioteka/Knjiga.java
@@ -1,6 +1,7 @@
 package bibilioteka;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Knjiga {
 	private String naslov;
@@ -47,6 +48,30 @@ public class Knjiga {
 
 	public void setAutori(List<Autor> autori) {
 		this.autori = autori;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(autori, isbn, izdanje, izdavac, naslov);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		return Objects.equals(autori, other.autori) && isbn == other.isbn && izdanje == other.izdanje
+				&& Objects.equals(izdavac, other.izdavac) && Objects.equals(naslov, other.naslov);
+	}
+
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
+				+ ", izdanje=" + izdanje + "]";
 	}
 
 }
